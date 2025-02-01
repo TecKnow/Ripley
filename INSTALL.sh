2d58acf8cfa121e5817c45308afbb00eb1f41f6e
David P. Perkins <tecknow@grumbleware.com>
2025-01-31 21:17:45 -0600

    Remove files specific to the previous method of display.

--- INSTALL.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -x
-
-pip install     -f https://extras.wxpython.org/wxPython4/extras/linux/gtk3/ubuntu-22.04     wxPython
- # attrdict
-# sudo apt-get install python-wxgtk2.8 xclip
-
-wget -N https://github.com/CarlFK/Ripley/raw/master/ripley.py
-wget -N https://github.com/CarlFK/Ripley/raw/master/ripley.txt
-wget -N https://github.com/CarlFK/Ripley/raw/master/e2a.py
